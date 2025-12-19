377c4d40c9e719070c0dbb255aa6aa76ae196133
KrishCodesw <krishjain.w@gmail.com>
2025-12-19 15:33:23 +0530

    2025-12-19 15:33:23

--- binarysearchalgo/LetterCeiling.java
+++ binarysearchalgo/LetterCeiling.java
@@ -1,3 +1,25 @@
-public class LetterCeiling {
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
 
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+
+    public static void main(String[] args) {
+        char[] letters = { 'a', 'c', 'h', 'l', 'n' };
+        char target = 'd';
+        Solution obj = new Solution();
+        char ans = obj.nextGreatestLetter(letters, target);
+        System.out.println(ans);
+
+    }
 }
