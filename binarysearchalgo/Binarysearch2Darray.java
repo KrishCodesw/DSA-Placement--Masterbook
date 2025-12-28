6e417bd9bf0c132076584e34662a56058c7f4e10
KrishCodesw <krishjain.w@gmail.com>
2025-12-28 17:09:54 +0530

    binary search in 2d array

---
+++ binarysearchalgo/Binarysearch2Darray.java
@@ -0,0 +1,31 @@
+
+import java.util.Arrays;
+
+public class Binarysearch2Darray {
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 10, 20, 30, 40 },
+                { 15, 25, 35, 45 },
+                { 28, 29, 37, 49 }
+        };
+        int target = 28;
+        int[] ans = search(arr, target);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] search(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix.length - 1;
+        while (row <= matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return new int[] { row, col };
+            }
+            if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+}
