49009b79d0def04baa7f47f9195a39316132307b
KrishCodesw <krishjain.w@gmail.com>
2026-04-02 02:09:59 +0530

    2026-04-02 02:09:59

---
+++ javacollectionframeworks/linkedlist/L1.java
@@ -0,0 +1,17 @@
+class Node {
+    public int value;
+    public Node next;
+}
+
+public class L1 {
+    public static void main(String[] args) {
+        Node node1 = new Node();
+        Node node2 = new Node();
+        node1.value = 1;
+        node1.next = node2;
+        node2.value = 2;
+        node2.next = null;
+    }
+}
+
+// This is not to be done
