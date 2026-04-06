5ab14770e9a6b8006ac3aeec8b057a0c702b90e3
KrishCodesw <krishjain.w@gmail.com>
2026-04-06 21:30:42 +0530

    2026-04-06 21:30:42

---
+++ javacollectionframeworks/CopyOnWriteArraylist/CoWAL.java
@@ -0,0 +1,8 @@
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CoWAL {
+    public static void main(String[] args) {
+        CopyOnWriteArrayList<Integer> co1 = new CopyOnWriteArrayList<>();
+
+    }
+}
