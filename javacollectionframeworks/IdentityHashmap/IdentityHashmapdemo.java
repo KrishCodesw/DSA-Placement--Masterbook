b662327fbebea33ce9ef8d55e69a22f41851b0b2
KrishCodesw <krishjain.w@gmail.com>
2026-04-12 13:33:52 +0530

    2026-04-12 13:33:52

--- javacollectionframeworks/IdentityHashmap/IdentityHashmapdemo.java
+++ javacollectionframeworks/IdentityHashmap/IdentityHashmapdemo.java
@@ -2,12 +2,16 @@ import java.util.HashMap;
 
 public class IdentityHashmapdemo {
     public static void main(String[] args) {
-        String Key1 = new String("key");
-        String Key2 = new String("key");
+        String Key1 = new String("key"); // iska address alag
+        String Key2 = new String("key"); // iska address bhi alag
 
         HashMap<String, Integer> map = new HashMap<>();
-        map.put(Key1, 1);
-        map.put(Key2, 2);
+        map.put(Key1, 1); // key,1
+        map.put(Key2, 2); // key,2
+        // Equal nahi hai toh collision aayega and agar equal hai toh replace hojeaga
+        // Equal will be checked with equals() method se
+        System.out.println(Key1.equals(Key2));
+        
         System.out.println(map);
     }
 }
